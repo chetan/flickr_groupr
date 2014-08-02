653350f8014a1430d11e64109ddc0e2232d995c0
Chetan Sarva <chetan@pixelcop.net>
2014-08-02 17:47:00 -0400

    500 per page; expand to last 3 days; be verbose about skipped photos as well

--- groupr.rb
+++ groupr.rb
@@ -25,31 +25,35 @@ class GrouprApp
     recent_time = Time.new-86400*7
     per_page = 500
     if File.exists?(LAST_RUN_FILE) then
-      recent_time = File.mtime(LAST_RUN_FILE)-(3600*3) # less a few hours of buffer time
-      per_page = 50
+      recent_time = File.mtime(LAST_RUN_FILE)-(86400*3) # less a few hours of buffer time
     end
 
     puts sprintf("Searching for new photos in album '%s' to add to '%s'", config["album"]["name"], config["group"]["name"]) if VERBOSE
 
-    photos = flickr.photosets.getPhotos(:photoset_id => config["album"]["id"],
+    res = flickr.photosets.getPhotos(:photoset_id => config["album"]["id"],
                                         :extras => "date_upload", :per_page => per_page)
+    total_pages = res["pages"]
 
-    photos = photos["photo"]
+    photos = res["photo"]
     photos.each do |photo|
       date = Time.at(photo["dateupload"].to_i)
-      if date >= recent_time then
-        # new photo! add to group
-        puts sprintf("* adding new photo '%s' (%s) to group", photo["title"], photo["id"]) if VERBOSE
-        begin
-          flickr.groups.pools.add(:photo_id => photo["id"], :group_id => config["group"]["id"])
-        rescue FlickRaw::FailedResponse => ex
-          if ex.message =~ /already/ then
-            puts sprintf("  (oops, photo was already in the group! moving on)") if VERBOSE
-          else
-            warn "  ERROR! #{ex.message}"
-            if !VERBOSE then
-              warn sprintf("         while adding photo '%s' (%s) to group '%s'", photo["title"], photo["id"], config["group"]["name"])
-            end
+      if date < recent_time then
+        # old photo
+        puts sprintf("* skipping photo %s: %s < %s", photo["id"], date, recent_time) if VERBOSE
+        next
+      end
+
+      # new photo! add to group
+      puts sprintf("* adding new photo '%s' (%s) to group", photo["title"], photo["id"]) if VERBOSE
+      begin
+        flickr.groups.pools.add(:photo_id => photo["id"], :group_id => config["group"]["id"])
+      rescue FlickRaw::FailedResponse => ex
+        if ex.message =~ /already/ then
+          puts sprintf("  (oops, photo was already in the group! moving on)") if VERBOSE
+        else
+          warn "  ERROR! #{ex.message}"
+          if !VERBOSE then
+            warn sprintf("         while adding photo '%s' (%s) to group '%s'", photo["title"], photo["id"], config["group"]["name"])
           end
         end
       end
